c8ece2abf9e328215446a2d4858729467cf63f8b
jaehyun429 <127477305+jaehyun429@users.noreply.github.com>
2025-05-04 21:44:57 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250504/DateTime to DateTime/datetime-to-datetime.py
+++ 250504/DateTime to DateTime/datetime-to-datetime.py
@@ -1,6 +1,11 @@
-a, b, c = map(int, input().split())
+# 변수 선언 및 입력
+a, b, c = tuple(map(int, input().split()))
 
+# 차이를 계산합니다.
+diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11)
 
-
-
-print((a-11)*24*60 + (b-11)* 60 + (c- 11))
+# 출력
+if diff < 0:
+    print(-1)
+else:
+    print(diff)
